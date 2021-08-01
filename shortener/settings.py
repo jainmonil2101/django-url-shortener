4a1947795f10d2d79b207722c42369b24bd011c4
Monil Jain <jainmonil2101@gmail.com>
2021-08-01 14:44:10 +0530

    Tweaked

--- shortener/settings.py
+++ shortener/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c9o8&s^2b&xn%$cl4er@&kile)j7ds!ijn8zmpni+bue6dr!@z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
