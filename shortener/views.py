c5e009ea7407a189773f8bc0989c915326b95e20
Monil Jain <58970445+jainmonil2101@users.noreply.github.com>
2021-10-22 16:13:50 +0530

    Update views.py

--- shortener/views.py
+++ shortener/views.py
@@ -1,17 +1,11 @@
 from django.shortcuts import render, redirect
 import pyshorteners
-from django.contrib.sites.models import Site
-
-current_site = Site.objects.get_current()
-
-
-
 
 def index(request):
     if request.method == 'POST':
         link = request.POST.get('link')
         shortener = pyshorteners.Shortener()
         result = shortener.tinyurl.short(link)
-        return render(request, 'index.html', {'result':result, 'domain':current_site.domain})
+        return render(request, 'index.html', {'result':result})
     else:
         return render(request, 'index.html')
